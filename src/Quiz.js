3a6aa72709fba5be9c62bb1190cb042ee01679a8
Zandraa <zandraa.perjesi@gmail.com>
2018-09-16 23:10:00 +0200

    Refactor code as per specifications

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,9 +9,7 @@ class Quiz extends Component {
     }
 
     render() {
-        return <div>
-            <div className='QuizQuestions'>{quizData.quiz_questions[0].instruction_text}</div>
-        </div>
+        return <div className='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div>
     }
 }
 
