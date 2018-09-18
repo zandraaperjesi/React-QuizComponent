f4e08944a76366850d8fd93547492fe810a4a735
Zandraa <zandraa.perjesi@gmail.com>
2018-09-18 23:01:13 +0200

    Add QuizEnd component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react' 
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -10,7 +11,11 @@ class Quiz extends Component {
     }
 
     render() {
-        return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+        const isQuizEnd = ((this.quiz_position - 1) === quizData.quiz_questions.length)
+        return <div>
+            {isQuizEnd ? <QuizEnd /> :
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+        </div>    
     }
 }
 
