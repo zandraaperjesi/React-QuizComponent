162b2366e99bedfbe203876496ae886ed6c4aae6
Zandraa <zandraa.perjesi@gmail.com>
2018-09-18 22:16:44 +0200

    Display proper instruction text

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ class QuizQuestion extends Component {
     render() {
         return <main>
         <section>
-          <p>{this.props.instruction_text}</p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
