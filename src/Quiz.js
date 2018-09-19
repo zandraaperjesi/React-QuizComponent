d1d5d1e77e4aa426e79975bf88db65991b70c446
Zandraa <zandraa.perjesi@gmail.com>
2018-09-19 22:35:07 +0200

    Add button interaction

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,11 +10,16 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return {quiz_position: state.quiz_position + 1}
+        })
+    }
+
     render() {
-        const isQuizEnd = ((this.quiz_position - 1) === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return <div>
-            {isQuizEnd ? <QuizEnd /> :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
         </div>    
     }
 }
