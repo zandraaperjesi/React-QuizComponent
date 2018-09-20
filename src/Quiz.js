535dac2ba371c2ea682feb9aa33114300f358596
Zandraa <zandraa.perjesi@gmail.com>
2018-09-20 22:36:38 +0200

    Add reset logic to quiz end

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,10 +16,14 @@ class Quiz extends Component {
         })
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1})
+    }
+
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return <div>
-            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
+            {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
         </div>    
     }
 }
